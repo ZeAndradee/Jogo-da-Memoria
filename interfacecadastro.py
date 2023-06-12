618d612f2a434736da048223ef37a76d3253fd58
mfhhia <nandahiarita.15@gmail.com>
2023-06-12 12:37:26 -0300

    tela de login concluida (só a interface)

--- interfacecadastro.py
+++ interfacecadastro.py
@@ -10,24 +10,17 @@ app.geometry("400x240")
 app.title('Cadastro')
 
 
-
-def abrir_login():
-  login = customtkinter.CTkToplevel()
-  login.title('login')
-  login.geometry('400x240')
-  app.close()
-
 def switch_theme():
       if switch_var.get() == "on":
         customtkinter.set_appearance_mode("dark")
       else:
         customtkinter.set_appearance_mode("light")   
 
-
+#tela de início
 label = customtkinter.CTkLabel(app, text="Bem Vindo", font=customtkinter.CTkFont(size=20, weight="bold"))
 label.place(x=150, y=30)
 
-button = customtkinter.CTkButton(app, text='Fazer Login', command=abrir_login)
+button = customtkinter.CTkButton(app, text='Fazer Login', command=lambda:abrir_tela_login())
 button.place(x=130, y= 100)
 
 button = customtkinter.CTkButton(app, text='Cadastre-se', command='')
@@ -37,4 +30,45 @@ switch_var = customtkinter.StringVar(value="off")
 switch = customtkinter.CTkSwitch(app, text="Dark Mode", command=switch_theme,
                                  variable=switch_var, onvalue="on", offvalue="off")
 switch.place(x= 260, y= 200)
+
+def abrir_tela_login():
+    # Criando uma nova janela com o Toplevel
+    login_window = customtkinter.CTkToplevel()
+    login_window.title("Tela de login")
+    login_window.geometry("400x240")
+
+    
+
+    # Criando uma label com o custom tkinter
+    label = customtkinter.CTkLabel(login_window, text="Tela login", font=customtkinter.CTkFont(size=20, weight="bold"))
+    label.place(x=150, y=30)
+    #label nome
+    label = customtkinter.CTkLabel(login_window, text="Apelido", font=customtkinter.CTkFont(size=12, weight="normal"))
+    label.place(x=80, y=75)
+    #Criando TextBox
+    textbox = customtkinter.CTkTextbox(login_window, width=250, height=30)
+    textbox.place(x= 75, y=100)
+    textbox.insert("0.0", "new text to insert") # insere na linha 0 caractere 0
+    text = textbox.get("0.0", "end") # obtém o texto da linha 0 caractere 0 até o final
+    textbox.delete("0.0", "end") # deleta todo o texto
+    textbox.configure(state="normal") # configura a caixa de texto para ser somente leitura
+
+    labelsenha = customtkinter.CTkLabel(login_window, text="Senha", font=customtkinter.CTkFont(size=12, weight="normal"))
+    labelsenha.place(x=80, y=145)
+
+    #Criando TextBox
+    textbox2 = customtkinter.CTkTextbox(login_window, width=250, height=30)
+    textbox2.place(x= 75, y=170)
+    textbox2.insert("0.0", "new text to insert") # insere na linha 0 caractere 0
+    text2 = textbox2.get("0.0", "end") # obtém o texto da linha 0 caractere 0 até o final
+    textbox2.delete("0.0", "end") # deleta todo o texto
+    textbox2.configure(state="normal") # configura a caixa de texto para ser somente leitura
+    
+
+
+#nome = tk.Label(login, 'digite seu nome')
+#label2 = customtkinter.CTkLabel(login,text='Login', font=customtkinter.CTkFont(size= 20, weight='bold'))
+#label2.place(x=150, y=30)
+#label2 = tk.Label(login,text='nome')
+
 app.mainloop()
