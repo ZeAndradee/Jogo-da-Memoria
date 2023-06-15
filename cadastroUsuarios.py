7df4a8c7142ef9e7f393826c68796b84394bbd19
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-06-15 20:42:10 -0300

    Pequenas mudanças

--- cadastroUsuarios.py
+++ cadastroUsuarios.py
@@ -40,63 +40,85 @@ def abrir_login():
     login_window.title("Tela de login")
     login_window.geometry("400x240")
 
-    # Criando uma label com o custom tkinter
+    # Label Titulo Login
     label = customtkinter.CTkLabel(login_window, text="Tela login", font=customtkinter.CTkFont(size=20, weight="bold"))
     label.place(x=150, y=10)
 
-    #label nome
+    #label Apelido
     label = customtkinter.CTkLabel(login_window, text="Apelido", font=customtkinter.CTkFont(size=12, weight="normal"))
     label.place(x=80, y=50)
 
-    #Criando TextBox
+    #Criando TextBox Login
     textbox = customtkinter.CTkTextbox(login_window, width=250, height=30, border_color= '#3b8ed0', border_width=2)
     textbox.place(x= 75, y=75)
     textbox.insert("0.0", "new text to insert") 
     textbox.delete("0.0", "end") 
     textbox.configure(state="normal")
 
-    def botao_entrar():
-      usuarioL = textbox.get("0.0", "end").rstrip()
-      #recebe o input do login usuario
-      with open("usuarios.txt", "r", encoding="utf-8") as u: 
-         for line in u:  
-            listaU = u.read().splitlines()
-            #o split esta aqui para dividir os nomes em linhas 
-            
-            print(listaU)
-            
-            #Váriaveis User
-            posicao = -1
-            tam = len(listaU)
 
-            if posicao <= tam:
-              posicao = listaU.index(usuarioL)
-            if usuarioL in listaU:
-               print('Usuario: ', usuarioL)
-               print('existe!')
-               u.close()
-               abrir_jogo()
-               return
-      print('Usuario: ', usuarioL)
-      print('Não existe')
-      u.close()
 
-    #Botão Entrar
-    button_login = customtkinter.CTkButton(login_window, text='Fazer Login', command=botao_entrar)
-    button_login.place(x=130, y= 190)
-
-    #Pesquisando usuarios
+    #Label Senha
 
     labelsenha = customtkinter.CTkLabel(login_window, text="Senha", font=customtkinter.CTkFont(size=12, weight="normal"))
     labelsenha.place(x=80, y=120)
 
-    #Criando TextBox
+    #Criando TextBox Senha
     textbox2 = customtkinter.CTkTextbox(login_window, width=250, height=30, border_color= '#3b8ed0', border_width=2)
     textbox2.place(x= 75, y=145)
     textbox2.insert("0.0", "new text to insert")
-    text2 = textbox2.get("0.0", "end") 
+     
     textbox2.delete("0.0", "end")
     textbox2.configure(state="normal") 
+
+    def botao_entrar():
+      usuarioL = textbox.get("0.0", "end").strip()
+      usuarioS = textbox2.get("0.0", "end")
+      #Verifica o input do login usuario
+      with open("usuarios.txt", "r", encoding="utf-8") as u:   
+        listaU = u.read().splitlines()
+        print(listaU)
+            
+        #Váriaveis User
+        posicaoL = -1
+        tam = len(listaU)
+
+      #Verifica o input do login usuario
+        if posicaoL <= tam:
+            posicaoL = listaU.index(usuarioL)
+        if usuarioL in listaU:
+            print('Usuario: ', usuarioL)
+            print('existe!')
+            u.close()
+            return
+      print('Usuario: ', usuarioL)
+      print('Não existe')
+      u.close()
+
+      #Verifica o input senha
+      with open("senhas.txt", "r", encoding="utf-8") as s:   
+        listaS = s.read().splitlines()
+        print(listaS)
+
+        #Variaveis de verificação
+        posicaoS = listaS.index(usuarioS)
+
+        #Verifica se a senha está na lista
+        if usuarioS in listaS:
+            print('Senha: ', usuarioS)
+            print('existe!')
+            s.close()
+            if posicaoS != posicaoL:
+               print('A senha não pertence ao usuario digitado.')
+            else:
+               print(f'A senha pertence ao usuario {listaU[posicaoL]}')   
+        
+      print('Senha: ', usuarioS)
+      print('Não existe')
+      u.close()
+
+    #Botão Fazer Login
+    button_login = customtkinter.CTkButton(login_window, text='Fazer Login', command=botao_entrar)
+    button_login.place(x=130, y= 190)   
 
 
 def abrir_cadastro():
@@ -156,6 +178,7 @@ def abrir_cadastro():
     button_cadastrar = customtkinter.CTkButton(cadastro_window, text= 'Cadastrar', command= botao_cadastrar)
     button_cadastrar.place(x=130, y= 190)
 
+#Def resposável por abrir a tela do jogo
 def abrir_jogo():
     # Criando uma nova janela com o Toplevel
     game_window = customtkinter.CTkToplevel()
